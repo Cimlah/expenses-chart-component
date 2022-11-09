ae1857528bc096b5ce43ee600ec2fb7f8710f4f4
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-11-09 21:24:12 +0100

    Changed script to calculate height of chart bars based on the highest value, not a sum

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
-import jsonData from '../data.json' assert {type: 'json'}
-
-let moneySum = 0
-for(let i = 0; i < 7; i++) {
-    moneySum += jsonData[i].amount
-}
-const moneySumHTML = document.querySelector('.total-this-month-amount')
-moneySumHTML.innerHTML = "$" + moneySum
+const jsonData = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48]
 
 const days = document.querySelectorAll('.day')
 days.forEach((day, index) => {
-    day.style.height = jsonData[index].amount/moneySum*100 + "%"
+    day.style.height = jsonData[index]/Math.max(...jsonData)*100 + "%"
 })
