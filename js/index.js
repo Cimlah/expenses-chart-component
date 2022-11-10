4cb276cb7a3d53e628017711b8d223cce81a69e2
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-11-10 22:12:18 +0100

    Added a functionality for putting values into pseudo elements using attributes

--- js/index.js
+++ js/index.js
@@ -3,4 +3,6 @@ const jsonData = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48]
 const days = document.querySelectorAll('.day')
 days.forEach((day, index) => {
     day.style.height = jsonData[index]/Math.max(...jsonData)*100 + "%"
+
+    day.setAttribute("amount", "$" + jsonData[index])
 })
